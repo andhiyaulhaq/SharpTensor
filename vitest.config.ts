741b07b1ec6b9f1e1f48baf7babee631c920c6f6
andhiyaulhaq <andhiyaulhaq@gmail.com>
2026-05-24 08:36:06 +0700

    chore: exclude e2e directory from vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/**'],
     setupFiles: ['./src/tests/setup.ts'],
     coverage: {
       provider: 'v8',
